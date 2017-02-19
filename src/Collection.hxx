9a3d4af06fa7a64d1fce44faa17bc9df1109c40f
root <root@ubuntu.(none)>
2017-02-19 20:09:22 +0100

    REFACTOR:CollectionTests::testTitle_addingTitle

--- src/Collection.hxx
+++ src/Collection.hxx
@@ -10,17 +10,19 @@ class Collection{
 	Collection();
 
 	//Methods
-	std::string title();
-	std::string title(std::string stringTitle);
+	std::string title();//Added in First test METHOD
+	std::string title(std::string stringTitle);//Added in Second test METHOD
 	
 };
 
 Collection::Collection(){
 		a = "-- Untitled --";		
 	}
+	//Added in First test METHOD
 	std::string Collection::title(){
 		return a;
 	}
+	//Added in Second test METHOD
 	std::string Collection::title(std::string stringTitle){
 		a = stringTitle;	
  		return a;
