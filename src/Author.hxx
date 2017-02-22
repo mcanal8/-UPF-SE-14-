49d0388f2c9280a76d500c39e586c573c7dcf4e8
Marc <marc.canal01@estudiant.upf.edu>
2017-02-22 11:06:04 +0100

    RED:AddAuthorAndWorkTests.cxx::testCatalogue_withoutWorks

---
+++ src/Author.hxx
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+
+class Author{
+
+	private:
+		
+
+	public:
+	//Constructor
+	Author();
+
+	//Methods
+	std::string catalogue();
+};
+
+Author::Author(){
+
+}
+
+std::string Author::catalogue(){
+	return 0;
+}
