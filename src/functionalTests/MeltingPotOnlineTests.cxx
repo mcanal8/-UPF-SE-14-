a6549b497fc7b292e68bcefcf0b77cafb2fd2d1d
Marc <marc.canal01@estudiant.upf.edu>
2017-04-15 17:45:33 +0200

    GREEN:MeltingPotOnlinetests::testCatalogue_withTwoAuthors

--- src/functionalTests/MeltingPotOnlineTests.cxx
+++ src/functionalTests/MeltingPotOnlineTests.cxx
@@ -10,7 +10,7 @@ public:
 	{
 		TEST_CASE( testCatalogue_withoutAuthors );
 		TEST_CASE( testCatalogue_withExternalAuthor );
-		//TEST_CASE( testCatalogue_withContractedAuthor );
+		TEST_CASE( testCatalogue_withContractedAuthor );
 		//TEST_CASE( testCatalogue_withTwoAuthors );
 		//TEST_CASE( testCatalogue_withWork );
 		//TEST_CASE( testCatalogue_withTwoWorksBySameAuthor );
@@ -64,7 +64,7 @@ public:
 			business.catalogue()
 		);
 	}
-	/*void testCatalogue_withContractedAuthor()
+	void testCatalogue_withContractedAuthor()
 	{
 		MeltingPotOnline business;
 		business.addAuthor( "A contracted author", true );
@@ -86,7 +86,7 @@ public:
 			business.catalogue()
 		);
 	}
-	void testCatalogue_withWork()
+	/*void testCatalogue_withWork()
 	{
 		MeltingPotOnline business;
 		business.addAuthor( "An author", false );
