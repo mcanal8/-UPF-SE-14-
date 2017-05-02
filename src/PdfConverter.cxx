abc3c7e3ef2eb16871b4177af0e31442146dc6e7
Marc <marc.canal01@estudiant.upf.edu>
2017-05-02 13:00:53 +0200

    GREEN:PdfConverterTests::testConvert_generateFile

--- src/PdfConverter.cxx
+++ src/PdfConverter.cxx
@@ -15,9 +15,28 @@
 	}
 
 void PdfConverter::convert(const char* original, std::string converted){
+		if(watermark_state){
+			//converted.append( " [watermark].pdf" );
+			const char* convertedName[]= {"generated/Prefix [watermark].pdf", 0
+			};
+
+			for(int i = 0; convertedName[i]; ++i)
+
+				std::ofstream newfile(convertedName[i]);
+			}
+		else{
+			//converted.append("  [printable].pdf");
+
+			const char* convertedName[]= {"generated/Prefix [printable].pdf", 0
+			};
+			for(int i = 0; convertedName[i]; ++i)
+				std::ofstream newfile(convertedName[i]);
+		}
 
 
 }
 void PdfConverter::activateWatermark(const std::string newWatermark){
+		watermark_state = true;
+		watermark = newWatermark;
 
 }	
---
+++ src/unitTests/PdfConverterTests.cxx
@@ -0,0 +1,108 @@
+#include <fstream>
+#include "LibFileSystem.hxx"
+#include "MiniCppUnit.hxx"
+#include "PdfConverter.hxx"
+
+
+
+class pdfConverterTests : public TestFixture<pdfConverterTests>
+{
+public:
+	TEST_FIXTURE( pdfConverterTests )
+	{
+		TEST_CASE( testConvert_generateFile );
+		//TEST_CASE( testConvert_generateContent );
+		//TEST_CASE( testConvert_withInexistentOriginal );
+		//TEST_CASE( testConvert_polymorphicCall );
+		
+	}
+
+	/**
+	 * The setUp method is automatically called before each test
+	 */
+	void setUp()
+	{
+		LibFileSystem::createNetDirectory( "config" );
+		LibFileSystem::createNetDirectory( "generated" );
+		LibFileSystem::createNetDirectory( "originals" );
+	}
+	/**
+	 * The tearDown method is automatically called after each test
+	 */
+	void tearDown()
+	{
+		LibFileSystem::cleanupDirectory( "config" );
+		LibFileSystem::cleanupDirectory( "generated" );
+		LibFileSystem::cleanupDirectory( "originals" );
+	}
+	/**
+	 * This method creates files used in tests
+	 */
+	void createOriginalFile( const std::string & name )
+	{
+		std::string fullname( "originals/" );
+		fullname += name;
+		std::ofstream os( fullname.c_str() );
+		os << "An original file" << std::endl;
+		os.close();
+	}
+	
+	void testConvert_generateFile()
+	{
+		PdfConverter converter;
+		createOriginalFile( "Original.odt" );
+		converter.convert( "originals/Original.odt", "generated/Prefix" );
+		converter.activateWatermark( "watermark" );
+		converter.convert( "originals/Original.odt", "generated/Prefix" );
+
+		ASSERT_EQUALS(
+			"generated/Prefix [printable].pdf\n"
+			"generated/Prefix [watermark].pdf\n",
+			LibFileSystem::listDirectoryInOrder( "generated" )
+			);
+	}
+
+	/*void testConvert_generateContent()
+	{
+		HtmlConverter converter;
+		createOriginalFile( "Original.odt" );
+		converter.convert( "originals/Original.odt", "generated/Prefix" );
+
+		ASSERT_EQUALS(
+			"War file generated from 'originals/Original.odt'\n",
+			LibFileSystem::fileContent( "generated/Prefix [multiple HTML files].war" )
+			);
+	}
+	
+	void testConvert_withInexistentOriginal()
+	{
+		HtmlConverter converter;
+		
+		try
+		{
+			converter.convert( "originals/Original.odt", "generated/Prefix" );
+			FAIL( "An exception should be caught!" );		
+		}
+		catch ( std::exception & e )
+		{
+			ASSERT_EQUALS(
+				"The original file does not exist",
+				e.what()
+			)
+		}	
+	}
+
+	/*void testConvert_polymorphicCall()
+	{
+		HtmlConverter instace;
+		Converter plymorphical;
+		polymorphical.convert()
+
+	}*/
+	
+	
+};
+
+REGISTER_FIXTURE(  pdfConverterTests )
+
+
