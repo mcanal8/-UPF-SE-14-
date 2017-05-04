fe56d9a36f29d0b2aa545ac136687d696e4da929
Marc <marc.canal01@estudiant.upf.edu>
2017-05-04 13:18:38 +0200

    GREEN:ConverterGroupTests::testConvert_withHtmlConverter

--- src/ConverterGroup.cxx
+++ src/ConverterGroup.cxx
@@ -1,4 +1,5 @@
 #include "ConverterGroup.hxx"
+#include "libLibreOffice2Html.hxx"
 
 ConverterGroup::ConverterGroup(){
 
@@ -14,5 +15,9 @@ void ConverterGroup::add(const std::string tipus){
 }
 
 void ConverterGroup::convert(const std::string &original, const std::string &converted){
-
+		std::string converted2;
+		converted2 = converted + " [multiple HTML files].war\0";
+		int conversion = 0;
+			std::ofstream newfile(converted2.c_str());
+			conversion = OO_WarGeneration(original.c_str(),converted2.c_str());
 }
