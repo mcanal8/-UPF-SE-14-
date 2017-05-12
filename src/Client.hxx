060925b08cc9f12f70458e2937bdbe24c1c9c400
Adrián Matilla <adrian.matilla01@estudiant.upf.edu>
2017-05-12 16:10:37 +0200

    Punt3 començat

---
+++ src/Client.hxx
@@ -0,0 +1,24 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+
+
+using namespace std;
+
+class Client{
+	//Atributos
+	private:
+		string name, email, _description;
+		
+	public:
+	
+	//constructor
+	Client();
+
+	//destructor
+	~Client();
+
+	//Methods
+	void setName(string clientName);
+	void setEmail(string 
+};
---
+++ src/unitTests/ClientTests.cxx
@@ -0,0 +1,61 @@
+#include <fstream>
+#include "LibFileSystem.hxx"
+#include "MiniCppUnit.hxx"
+
+
+
+
+
+class clientTests : public TestFixture<clientTests>
+{
+public:
+	TEST_FIXTURE( clientTests )
+	{
+		TEST_CASE(client_withNoData);
+		TEST_CASE(client_Description);
+		
+	}
+
+	/**
+	 * The setUp method is automatically called before each test
+	 */
+	void setUp()
+	{
+		LibFileSystem::createNetDirectory( "config" );
+		LibFileSystem::createNetDirectory( "generated" );
+		LibFileSystem::createNetDirectory( "originals" );
+	}
+	/**
+	 * The tearDown method is automatically called after each test
+	 */
+	void tearDown()
+	{
+		LibFileSystem::cleanupDirectory( "config" );
+		LibFileSystem::cleanupDirectory( "generated" );
+		LibFileSystem::cleanupDirectory( "originals" );
+	}
+
+	
+	
+	void client_withNoData()
+	{
+		Client client;
+
+		ASSERT_EQUALS( "", client.description() );
+	}
+	void client_Description()
+	{
+		Client client;
+		client.setName("A client");
+		client.setEmail("a@mail.org");
+
+		ASSERT_EQUALS( "A client <a@mail.org>\n", client.description() );
+
+
+	
+	
+};
+
+REGISTER_FIXTURE(  topicTests )
+
+
